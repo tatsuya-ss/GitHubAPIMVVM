bed19b4911abd51dc1ac2314c32e58f206339800
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-02-01 13:38:30 +0900

    API叩く処理

--- app/src/main/java/com/example/githubapimvvm/MainActivity.kt
+++ app/src/main/java/com/example/githubapimvvm/MainActivity.kt
@@ -6,17 +6,22 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubapimvvm.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 class GitHubModel(val name: String)
 data class GitHubEntity(val name: String)
 interface GitHubClient{
-    @GET("users/tatsuya-ss/")
+    @GET("users/tatsuya-ss")
     fun fetchUser(): Call<GitHubEntity>
 }
-
-//https://api.github.com/
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +32,12 @@ class MainActivity : AppCompatActivity() {
 
         setupBinding()
         setupRecyclerView()
+
+        GlobalScope.launch {
+            launch {
+                fetchGitHubUser()
+            }
+        }
 
     }
 
@@ -39,6 +50,38 @@ class MainActivity : AppCompatActivity() {
         binding.RecyclerView.layoutManager = LinearLayoutManager(this)
     }
 
+    private fun makeOkHttp(): OkHttpClient.Builder {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor{ chain ->
+            val request = chain.request().newBuilder().build()
+            val reponse = chain.proceed(request)
+            return@addInterceptor reponse
+        }
+        return httpClient
+    }
+
+    private fun createService(): GitHubClient {
+        val client = makeOkHttp().build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        val githubClient = retrofit.create(GitHubClient::class.java)
+        return githubClient
+    }
+
+    private fun fetchGitHubUser() {
+        createService().fetchUser().enqueue(object : retrofit2.Callback<GitHubEntity> {
+            override fun onResponse(call: Call<GitHubEntity>, response: Response<GitHubEntity>) {
+                println(response.body().toString())
+            }
+
+            override fun onFailure(call: Call<GitHubEntity>, t: Throwable) {
+                println(t.message)
+            }
+        })
+    }
 }
 
 class GitHubViewModel: ViewModel() {
